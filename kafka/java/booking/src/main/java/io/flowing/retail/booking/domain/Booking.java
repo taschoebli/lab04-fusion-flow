f566dc7b216bc625c30bb6006045068dd79e09ab
David <david.seger@adon.li>
2024-03-13 20:42:17 +0100

    add camunda to existing project

--- kafka/java/booking/src/main/java/io/flowing/retail/booking/domain/Booking.java
+++ kafka/java/booking/src/main/java/io/flowing/retail/booking/domain/Booking.java
@@ -33,11 +33,26 @@ public class Booking {
     return null;
   }
 
+  public String getOrderId() {
+    return orderId;
+  }
+  public void setOrderId(String orderId) {
+    this.orderId = orderId;
+  }
+  public List<Item> getItems() {
+    return items;
+  }
+  public void setItems(List<Item> items) {
+    this.items = items;
+  }
   @Override
   public String toString() {
     return "Order [orderId=" + orderId + ", items=" + items + "]";
   }
 
+  public Customer getCustomer() {
+    return customer;
+  }
 
   public void setCustomer(Customer customer) {
     this.customer = customer;
