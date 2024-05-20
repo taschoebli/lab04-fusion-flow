15342db571db184ae04bd32c224a74c0f7025b5d
Luzi Schoeb <>
2024-05-20 10:52:35 +0200

    replaced locationId with locationString

--- kafka/java/reporting/src/main/java/io/flowing/retail/reporting/helpers/Constants.java
+++ kafka/java/reporting/src/main/java/io/flowing/retail/reporting/helpers/Constants.java
@@ -11,4 +11,13 @@ public class Constants {
 
     public static String DATE_TIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
 
+    public static String locationIdToLocationString(Integer locationId) {
+        return switch (locationId) {
+            case 1, 11 -> Constants.LOCATION_ZUERICH;
+            case 2 -> Constants.LOCATION_BERN;
+            case 3, 30, 31 -> Constants.LOCATION_STGALLEN;
+            default -> Constants.INVALID_LOCATION;
+        };
+    }
+
 }
